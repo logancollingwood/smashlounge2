784ce1cc9481ee419fef0bf22dd3b2a4ff1056e8
Logan Collingwood <logan.collingwood@gmail.com>
2015-09-27 15:34:35 -0700

    Twitter card revamp for techniques

--- resources/views/modules/techs/showTech.blade.php
+++ resources/views/modules/techs/showTech.blade.php
@@ -1,19 +1,19 @@
 @extends('application')
 
 @section('twittercard')
-	<meta name="twitter:card" content="summary">
+	<meta name="twitter:card" content="player">
 	<meta name="twitter:site" content="@thesmashlounge">
 	<meta name="twitter:title" content="{{ $tech->tech }}">
 	<meta name="twitter:description" content="{{ $tech->description }}">
-	<!--
-	<meta name="twitter:image" content="https://yoursite.com/example.png">
-	<meta name="twitter:player" content="http://gfycat.com/AgileWatchfulIndianringneckparakeet">
-	
-	<meta name="twitter:player:width" content="480">
-	<meta name="twitter:player:height" content="480">
-  	<meta name="twitter:player:stream" content="http://devimages.apple.com/iphone/samples/bipbop/gear1/prog_index.m3u8">
-  	<meta name="twitter:player:stream:content_type" content="video/mp4; codecs=&quot;avc1.42E01E1, mp4a.40.2&quot;">
-	-->
+	<meta name="twitter:image" content="https://thumbs.gfycat.com/{{ $gifs[0]->url }}-poster.jpg">
+
+	<meta name="twitter:player" content="https://gfycat.com/ifrt/{{ $gifs[0]->url }}">
+	<meta name="twitter:player:width" content="1280">
+	<meta name="twitter:player:height" content="720">
+
+	<meta name="twitter:player:stream" content="https://giant.gfycat.com/{{ $gifs[0]->url }}.mp4"> 
+	<meta name="twitter:player:stream:content_type" content="video/mp4">
+
 @endsection
 
 @section('content')
