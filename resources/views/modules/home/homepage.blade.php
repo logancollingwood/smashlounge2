33641c93e24f6125db07ca9ea0b7d43bd2081482
logancollingwood <logan6694@gmail.com>
2015-10-20 15:24:15 -0700

    Rearranging homepage kinda

--- resources/views/modules/home/homepage.blade.php
+++ resources/views/modules/home/homepage.blade.php
@@ -23,12 +23,6 @@
 
 					@endforeach
 				</div>
-			</div>
-
-			<div class="col-md-4">
-				<a class="twitter-timeline" href="https://twitter.com/thesmashlounge" data-widget-id="585544387632967681">Tweets by @thesmashlounge</a>
-				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-				<hr>
 				<div class='panel-default streampanel'>
 					<div class='panel-heading'> Live SSB Streams </div>
             		<div class='panel-body streams'>
@@ -39,6 +33,12 @@
 						@endforeach
 					</div>
 				</div>
+			</div>
+
+			<div class="col-md-4">
+				<a class="twitter-timeline" href="https://twitter.com/thesmashlounge" data-widget-id="585544387632967681">Tweets by @thesmashlounge</a>
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+				<hr>
 
 			</div>
 
