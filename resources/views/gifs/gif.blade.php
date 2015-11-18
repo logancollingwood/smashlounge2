e3302bc0c842f44e32c9aea3ff9bf405753f3659
logan collingwood <logan6694>
2015-11-18 17:50:26 -0500

    reordering gif display

--- resources/views/gifs/gif.blade.php
+++ resources/views/gifs/gif.blade.php
@@ -1,21 +1,18 @@
 <div class="col-md-12">
 	<div class="gfyTainer">
 		<div class="row">
-			<div class="col-md-4 col-sm-3 voteBlock">
-				<div class="gfyDescription">{{ $gif->description or 'No Description'}}</div>
-					
-					<div class="gfySource">
-
-						@if ($gif->source && filter_var($gif->source, FILTER_VALIDATE_URL))
-							<a href="{{ $gif->source }}">source</a>
-						@elseif ($gif->source != "")
-							courtesy of {{ $gif->source }}
-						@endif
-
-					</div>
-			</div>
-			 <div class="col-md-8 col-sm-9">
+			<div class="col-md-8 col-sm-9">
 				<img class='gfyitem' data-autoplay=false data-expand=true data-id= {{ $gif->url}} />
+			</div>
+			<div class="col-md-4 col-sm-3 voteBlock">
+				<div class="gfyDescription"> {{$gif->description or 'No Description available'}}</div>
+				<div class="gfySource">
+					@if ($gif->source && filter_var($gif->source, FILTER_VALIDATE_URL))
+						<a href="{{$gif->source}}">source</a>
+					@elseif ($gif->source != "") 
+						from {{$gif->source}}
+					@endif
+				</div>
 			</div>
 		</div>
 	</div>
