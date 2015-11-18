50d48dc6ee5d501e70b632895948d9c9d8ce378f
logancollingwood <logan6694@gmail.com>
2015-11-18 15:03:31 -0800

    broken new gfycat api :(

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -130,14 +130,6 @@ Questions?
 	
 	<script src="{{ asset('/js/lib/bootstrap.min.js') }}"></script>
 	
-	<script>
-	 (function(d, t) {
-	    var g = d.createElement(t),
-	        s = d.getElementsByTagName(t)[0];
-	    g.src = 'http://assets.gfycat.com/js/gfyajax-0.517d.js';
-	    s.parentNode.insertBefore(g, s);
-	}(document, 'script'));
-	</script>
 	<!-- application minified js -->
 	<script src="{{ asset('/js/compiled/app.js') }}"></script>
 	
