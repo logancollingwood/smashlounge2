296b9087ebb2b4b4a9e3f8005099258e307dc2b4
logancollingwood <logan6694@gmail.com>
2015-11-18 14:57:53 -0800

    testing new gfycat ajax script

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -132,7 +132,14 @@ Questions?
 	
 	<!-- application minified js -->
 	<script src="{{ asset('/js/compiled/app.js') }}"></script>
-	
+	<script>
+	 (function(d, t) {
+	    var g = d.createElement(t),
+	        s = d.getElementsByTagName(t)[0];
+	    g.src = 'http://assets.gfycat.com/js/gfyajax-0.517d.js';
+	    s.parentNode.insertBefore(g, s);
+	}(document, 'script'));
+	</script>
 
 	
 
