16d3f1c290a00d4667ba7c840780e791c049c868
logancollingwood <logan6694@gmail.com>
2016-02-01 13:32:47 -0800

    Fixing this

--- resources/views/modules/moderate/grouptable.blade.php
+++ resources/views/modules/moderate/grouptable.blade.php
@@ -9,27 +9,29 @@
             <th>Approve</th>
             <th>Deny</th>
       	</tr>
+        @foreach ($data as $row)
+         
+        <tr class="submission" data-id="{{ $row->id }}">
+          <td>
+            {{ $row->name }}
+          </td>
+          <td>
+            <a href="{{ $row->facebook }}"> facebook </a>
+          </td>
+          <td>
+            <img src="http://maps.googleapis.com/maps/api/staticmap?center={{$row->latitude}},{{$row->longitude}}&zoom=3&size=50x50&key=AIzaSyBwejAOR-dv6OP1L2ttLguJOKA8Gzxi_lI">
+            {{ $row->latitude }}, {{ $row->longitude }}
+          </td>
+          <td>
+            {{ $row->region }}
+          </td>
+          <td>
+            {{ $row->game }}
+          </td>
+          
+          <td><a class="approve-link" href="#"><i class="fa fa-check-circle fa-3x"></i></a></td>
+          <td><a class="delete-link" href="#"><i class="fa fa-times-circle fa-3x"></i></a></td>
+        </tr>
+    @endforeach
   	</table>
-    @foreach ($data as $row)
-            <tr class="submission" data-id="{{ $row->id }}">
-              <td>
-                {{ $row->name }}
-              </td>
-              <td>
-                {{ $row->fb }}
-              </td>
-              <td>
-                {{ $row->lat }}, {{ $row->long }}
-              </td>
-              <td>
-                {{ $row->region }}
-              </td>
-              <td>
-                {{ $row->game }}
-              </td>
-              
-              <td><a class="approve-link" href="#"><i class="fa fa-check-circle fa-3x"></i></a></td>
-              <td><a class="delete-link" href="#"><i class="fa fa-times-circle fa-3x"></i></a></td>
-            </tr>
-        @endforeach
 </div>
